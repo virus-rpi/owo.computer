1ae1ad018112b9fe555a13896052486509792342
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2025-02-14 21:18:07 +0100

    feat:  add more emotions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,32 @@ import styles from "./page.module.css";
 export default function Home() {
   const [text, setText] = useState("OwO");
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
+  const [currentSet, setCurrentSet] = useState<keyof typeof variants>("happy");
+
+  const variants = {
+    happy: ["OwO", "UwU", "owo", "uwu", ">w<", "^w^"],
+    sad: ["TwT", "QwQ", "QnQ", "TnT"],
+    colon3: [":3", ":0", ":D", ":P", ":c", ":v", ":o", ":O", ">:3"],
+  };
 
   const handleMouseOver = () => {
-    const variations = ["OwO", "UwU", "owo", "uwu", ">w<", "^w^"];
-    setIntervalId( setInterval(() => {
-      const randomVariation = variations[Math.floor(Math.random() * variations.length)];
+    setIntervalId(setInterval(() => {
+      const randomVariation = variants[currentSet][Math.floor(Math.random() * variants[currentSet].length)];
       setText(randomVariation);
-    }, 50))
+    }, 50));
   };
 
   const handleMouseOut = () => {
     clearInterval(intervalId);
+  };
+
+  const handleClick = () => {
+    const keys = Object.keys(variants) as Array<keyof typeof variants>;
+    const currentIndex = keys.indexOf(currentSet);
+    const nextIndex = (currentIndex + 1) % keys.length;
+    setCurrentSet(keys[nextIndex]);
+    const randomVariation = variants[currentSet][Math.floor(Math.random() * variants[currentSet].length)];
+      setText(randomVariation);
   };
 
   return (
@@ -25,6 +40,7 @@ export default function Home() {
           className={styles.owo}
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
+          onClick={handleClick}
         >
           {text}
         </div>
