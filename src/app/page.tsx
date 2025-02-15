1bd382ce8233d42ec68dee587d1ce69ff384ad20
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2025-02-15 23:19:48 +0100

    feat: more variants feat: better look feat: new control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, {useState, useEffect, useMemo} from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -7,11 +7,11 @@ export default function Home() {
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
   const [currentSet, setCurrentSet] = useState<keyof typeof variants>("happy");
 
-  const variants = {
+  const variants = useMemo(() => ({
     happy: ["OwO", "UwU", "owo", "uwu", ">w<", "^w^"],
     sad: ["TwT", "QwQ", "QnQ", "TnT"],
-    colon3: [":3", ":0", ":D", ":P", ":c", ":v", ":o", ":O", ">:3"],
-  };
+    colon3: [":3", ":0", ":D", ":P", ":c", ":v", ":o", ":O", ">:3", ":3c", ">:3c"],
+  }), []);
 
   const handleMouseOver = () => {
     setIntervalId(setInterval(() => {
@@ -24,23 +24,35 @@ export default function Home() {
     clearInterval(intervalId);
   };
 
-  const handleClick = () => {
+  useEffect(() => {
+    const switchVariation = () => {
     const keys = Object.keys(variants) as Array<keyof typeof variants>;
-    const currentIndex = keys.indexOf(currentSet);
-    const nextIndex = (currentIndex + 1) % keys.length;
-    setCurrentSet(keys[nextIndex]);
-    const randomVariation = variants[currentSet][Math.floor(Math.random() * variants[currentSet].length)];
+      const currentIndex = keys.indexOf(currentSet);
+      const nextIndex = (currentIndex + 1) % keys.length;
+      setCurrentSet(keys[nextIndex]);
+      const randomVariation = variants[keys[nextIndex]][Math.floor(Math.random() * variants[keys[nextIndex]].length)];
       setText(randomVariation);
+  }
+    const onKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "s") {
+      switchVariation();
+    }
   };
+    window.addEventListener("keydown", onKeyDown);
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [variants, currentSet]);
 
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <main>
         <div
           className={styles.owo}
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
-          onClick={handleClick}
+          onFocus={handleMouseOver}
+          onBlur={handleMouseOut}
         >
           {text}
         </div>
